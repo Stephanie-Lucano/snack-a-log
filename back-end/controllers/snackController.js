05f5f33516e33b090fb4e55e612be608d081c642
justin <justin.d.diaz@gmail.com>
2022-02-11 15:57:36 -0500

    get one snack done

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const snacks = express();
-const { getAllSnacks } = require("../queries/snacks");
+const { getAllSnacks, getSnack } = require("../queries/snacks");
 
 snacks.get("/", async (_, response) => {
   console.log("GET request to /snacks");
   const allSnacks = await getAllSnacks();
   response.status(200).json(allSnacks);
+});
+
+snacks.get("/:id", async (request, response) => {
+  console.log("GET request to /snacks/:id");
+  const getOne = await getSnack(request.params.id);
+  response.status(200).json(getOne);
 });
 
 module.exports = snacks;
