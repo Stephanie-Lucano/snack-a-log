db2ada1ff3faffc15973eb797be7eb17c4008892
justin <justin.d.diaz@gmail.com>
2022-02-11 12:45:19 -0500

    set up routes

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,23 @@
 // DEPENDENCIES
 
 const express = require("express");
+const snacksController = require("./controllers/snackController");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use("/snacks", snacksController);
 
 // ROUTES
 
+app.get("/", (_, response) => {
+  console.log("GET request to /");
+  response.status(200).send("Hello world!");
+});
+
+app.get("*", (_, response) => {
+  response.status(404).send("Page not found");
+});
 // EXPORT
 module.exports = app;
