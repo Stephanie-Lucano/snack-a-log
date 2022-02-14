e4eb737ce688c76ff378abd72613bf232ac1aabd
justin <justin.d.diaz@gmail.com>
2022-02-14 10:51:12 -0500

    error handleing

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -20,37 +20,46 @@ const getSnack = async (id) => {
 
 const deleteSnack = async (id) => {
   try {
-    const deleteOne = await db.one("DELETE FROM snacks WHERE id=$1 RETURNING *", id)
-    return deleteOne
+    const deleteOne = await db.one(
+      "DELETE FROM snacks WHERE id=$1 RETURNING *",
+      id
+    );
+    return deleteOne;
   } catch (error) {
-    return error
+    return error;
   }
-}
+};
 
 const updateSnack = async (id, snacks) => {
   try {
-    const {name, fiber, protein, added_sugar, is_healthy, image} = snacks
-    const update = await db.one("UPDATE snacks SET name=$2, fiber=$3, protein=$4, added_sugar=$5, is_healthy=$6, image=$7 WHERE id=$1 RETURNING *", [id, name, fiber, protein, added_sugar, is_healthy, image])
-    return update
+    const { name, fiber, protein, added_sugar, is_healthy, image } = snacks;
+    const update = await db.one(
+      "UPDATE snacks SET name=$2, fiber=$3, protein=$4, added_sugar=$5, is_healthy=$6, image=$7 WHERE id=$1 RETURNING *",
+      [id, name, fiber, protein, added_sugar, is_healthy, image]
+    );
+    return update;
   } catch (error) {
-    return error
+    return error;
   }
-}
+};
 
 const postSnack = async (snacks) => {
   try {
-    const {name, fiber, protein, added_sugar, is_healthy, image} = snacks
-    const post = await db.one("INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *", [name, fiber, protein, added_sugar, is_healthy, image])
-    return post
+    const { name, fiber, protein, added_sugar, is_healthy, image } = snacks;
+    const post = await db.one(
+      "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [name, fiber, protein, added_sugar, is_healthy, image]
+    );
+    return post;
   } catch (error) {
-    return error
+    return error;
   }
-}
+};
 
 module.exports = {
   getAllSnacks,
   getSnack,
   deleteSnack,
   updateSnack,
-  postSnack
+  postSnack,
 };
